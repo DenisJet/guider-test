94e1e330070798ea759b8d2dd378cb923ec14f00
Denis Kalkopf <deniskalkopf@gmail.com>
2024-06-11 12:03:15 +0500

    fix

--- src/components/Service/Service.tsx
+++ src/components/Service/Service.tsx
@@ -1,9 +1,18 @@
+import { ServiceList } from '../ServiceList/ServiceList';
 import styles from './Service.module.css';
 
 export function Service(): JSX.Element {
   return (
-    <section>
-      <h2>Сервис</h2>
+    <section className={styles.service}>
+      <div className={styles.serviceTextContainer}>
+        <h2 className={styles.title}>Сервис</h2>
+        <p className={styles.slogan}>От идеи до незабываемого и измеримого результата.</p>
+        <div className={styles.divider}></div>
+        <p className={styles.text}>
+          Интеграция безупречного производства, передовых технологий и тщательного измерения производительности
+        </p>
+      </div>
+      <ServiceList />
     </section>
   );
 }
